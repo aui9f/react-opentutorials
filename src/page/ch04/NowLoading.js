949940af0daf61e7a4abcf7cd1c90bf948bea92a
aui9f <aui9f@naver.com>
2022-05-25 00:10:57 +0900

    로딩 중 기능 구현

---
+++ src/page/ch04/NowLoading.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class NowLoading extends Component{
+    render() {
+        return(
+            <div>
+                <p>Loading..</p>
+            </div>
+        )
+    }
+}
+export default NowLoading;
--- src/page/ch04/index.js
+++ src/page/ch04/index.js
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Article from "./Article";
 import Nav from "./Nav";
+import NowLoading from "./NowLoading";
 function Index(){
     const [status, setStatus] = useState({title: 'Welcom', desc: 'Hello, React & Ajax'});
 
     const [list, setList] = useState([]);
-    
+    const [isLoading, setIsLoading] = useState(false)
     
     const navClick = (id) => {
         
-        fetch(`${id}.json`).then(res=>{
-            return res.json();
-        }).then((json)=>{
-            setStatus({
-                title: json.title,
-                desc: json.desc
+            fetch(`${id}.json`).then(res=>{
+                return res.json();
+            }).then((json)=>{
+                setStatus({
+                    title: json.title,
+                    desc: json.desc
+                })
             })
-        })
+        
     }
 
     const test = async () => {
-        const result = await fetch('list.json');
-        // const json = ;
-        setList([...await result.json()]);
+        setIsLoading(true);
+        setTimeout(async()=>{
+            const result = await fetch('list.json');
+            setList([...await result.json()]);
+            setIsLoading(false);
+        },2000)
+        
         
     }
 useEffect(()=>{
@@ -30,9 +36,12 @@ useEffect(()=>{
 }, [])
     return(
         <div>
+            {isLoading?<NowLoading/>:<div> 
+                <Nav list={list} onClick={navClick}/>
+                <Article title={status.title} desc={status.desc}/>
+        </div>}
             
-            <Nav list={list} onClick={navClick}/>
-            <Article title={status.title} desc={status.desc}/>
+           
         </div>
     )
 
